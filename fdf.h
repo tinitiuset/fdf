25201da12de6a801524d20ae89080e56598be7c6
tinitiuset <tinitiuset@gmail.com>
2022-11-30 12:46:54 +0100

    Created header file

---
+++ fdf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 12:41:23 by mvalient          #+#    #+#             */
+/*   Updated: 2022/11/30 12:44:38 by mvalient         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "math.h"
+# include "MLX42.h"
+
+# include "./libft_extra/ft_printf/ft_printf.h"
+# include "./libft_extra/get_next_line/get_next_line.h"
+# include "./libft_extra/libft/libft.h"
+
+# define WIDTH 800
+# define HEIGHT 600
+
+#endif
