6860a86ce35354c6ce3f9d061a9b98154903f534
tinitiuset <tinitiuset@gmail.com>
2022-12-21 13:53:06 +0100

    Added protection for negative pixel values.

--- pixel_utils.c
+++ pixel_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <mvalient@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 13:47:50 by mvalient          #+#    #+#             */
-/*   Updated: 2022/12/21 13:12:37 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/12/21 13:26:33 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_put_pixel(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	if (x > WIDTH || y > HEIGHT)
+	if (x > WIDTH || y > HEIGHT || x < 0 || y < 0)
 		return ;
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *) dst = color;
